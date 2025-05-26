19c4cc42fd5b0a9a72b716b30b20476b81b40fb0
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2025-05-26 19:57:20 -0400

    10

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {FaMoon, FaSun} from 'react-icons/fa'
+//import {FaMoon, FaSun} from 'react-icons/fa'
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 import { useGlobalContext } from './Context'
 
 const ThemeToggle = () => {
@@ -7,7 +8,7 @@ const ThemeToggle = () => {
 
   return (
     <div className='toggle-container '>
-      <button className='toggle-icon' onClick={()=>{toggleChanger()}}>{toogleTrue?<FaMoon/>:<FaSun/>}</button>
+      <button className='toggle-icon' onClick={()=>{toggleChanger()}}>{toogleTrue?<BsFillMoonFill/>:<BsFillSunFill/>}</button>
     </div>
   )
 }
