664b563826d0f22c0252287e07adc92c05bfa6e5
Jose Hernandez <josehernandez@Joses-MacBook-Pro.local>
2025-05-26 20:22:14 -0400

    11

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-//import {FaMoon, FaSun} from 'react-icons/fa'
+import {FaMoon} from 'react-icons/fa'
+import {FaSun} from 'react-icons/fa'
 import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 import { useGlobalContext } from './Context'
 
@@ -8,7 +9,7 @@ const ThemeToggle = () => {
 
   return (
     <div className='toggle-container '>
-      <button className='toggle-icon' onClick={()=>{toggleChanger()}}>{toogleTrue?<BsFillMoonFill/>:<BsFillSunFill/>}</button>
+      <button className='toggle-icon' onClick={()=>{toggleChanger()}}>{toogleTrue?<FaMoon/>:<FaSun/>}</button>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa']
-    }
+  optimizeDeps: {
+    include: ['react-icons/fa', 'react-icons/bs']
   }
 })
